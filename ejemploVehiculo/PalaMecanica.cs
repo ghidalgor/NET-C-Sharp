940c5fbc0f6b135af2a64da292c89b460bd6c972
ghidalgor <ghidalgor@gmail.com>
2015-09-08 10:46:01 -0600

    Solo test

---
+++ ejemploVehiculo/PalaMecanica.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemploVehiculo
+{
+    class PalaMecanica:vehiculo
+    {
+        public void LevantarPala()
+        {
+            Console.WriteLine("Clase PalaMecanica, Método Levantar Pala");
+        }
+
+
+        public void BajarrPala()
+        {
+            Console.WriteLine("Clase PalaMecanica, Método Bajar Pala");
+        }
+    }
+}
